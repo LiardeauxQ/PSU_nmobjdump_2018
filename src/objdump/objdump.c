145a182b492ed65a921e91f7bc58673ca5894c7f
Quentin Liardeaux <quentin.liardeaux@epitech.eu>
2019-02-15 14:58:31 +0100

    Add -s option that print the contents of the sections, start to add some error managment when there is no file

--- src/objdump/objdump.c
+++ src/objdump/objdump.c
@@ -7,73 +7,25 @@
 
 #include "objdump.h"
 
-void print_help(void)
+int objdump(char *filename)
 {
-    printf("Usage: objdump <option(s)> <file(s)>\n\
- Display information from object <file(s)>.\n\
- At least one of the following switches must be given:\n\
-  -f, --file-headers       Display the contents of the overall file header\n\
-  -s, --full-contents      Display the full contents of all sections\
-requested\n\
-  -H, --help               Display this information\n");
-}
+    void *data = stock_file(filename);
+    int ret = 0;
 
-char **find_filenames(char **av, char *options[])
-{
-    char **filenames = NULL;
-    size_t size = 1;
-    char is_option = 0;
-
-    for (size_t i = 1 ; av[i] != NULL ; i++) {
-        for (size_t j = 0 ; options[j] != NULL ; j++) {
-            if (!strcmp(av[i], options[j]))
-                is_option = 1;
-        }
-        if (is_option == 0) {
-            filenames = realloc(filenames, (size + 1) * sizeof(char*));
-            filenames[size - 1] = av[i];
-            filenames[size] = NULL;
-            size = size + 1;
-        }
-        is_option = 0;
-    }
-    return (filenames);
-}
-
-int call_options(char **filenames, int index)
-{
-    int (*func[4])(void *, char *) = {&display_sections_content,
-        &display_sections_content, &display_fill_header, &display_fill_header};
-    void *data = NULL;
-
-    if (index >= 4)
-        return (-1);
-    for (size_t i = 0 ; filenames[i] != NULL ; i++) {
-        data = stock_file(filenames[i]);
-        if (data == NULL)
-            continue;
-        func[index](data, filenames[i]);
-    }
-    return (0);
+    if (data == NULL)
+        return (84);
+    ret = display_fill_header(data, filename);
+    ret = (display_sections_content(data, filename) == 0) ? ret : 84;
+    return (ret);
 }
 
 int main(int ac, char **av)
 {
-    char *options[] = {"-s", "--full-contents", "-f", "--file-headers", NULL};
-    char **filenames = NULL;
+    int ret = 0;
 
-    if (ac < 3) {
-        print_help();
-        return (-1);
-    }
-    filenames = find_filenames(av, options);
-    for (size_t i = 1 ; av[i] != NULL ; i++) {
-        for (size_t j = 0 ; options[j] != NULL ; j++) {
-            if (!strcmp(av[i], options[j])) {
-                call_options(filenames, j);
-            }
-        }
-    }
-    free(filenames);
-    return (0);
+    if (ac == 1)
+        return (objdump("a.out"));
+    for (size_t i = 1 ; av[i] != NULL ; i++)
+        ret = (objdump(av[i]) == 0) ? ret : 84;
+    return (ret);
 }
--- src/objdump/options_functions.c
+++ src/objdump/options_functions.c
@@ -6,7 +6,6 @@
 */
 
 #include "objdump.h"
-#include "flags.h"
 
 static char const *format[] = {"x86", "x86-64"};
 
@@ -25,7 +24,63 @@ int display_fill_header(void *data, char *filename)
     return (0);
 }
 
+static void print_byte_values(Elf64_Shdr *section, void *data, int pos)
+{
+    unsigned char *byte = NULL;
+
+    for (size_t j = 0 ; j < 16 ; j++) {
+        if (j % 4 == 0)
+            printf(" ");
+        if (j + pos >= section->sh_size) {
+            printf("  ");
+            continue;
+        }
+        byte = data + section->sh_offset + pos + j;
+        printf("%02x", *byte);
+    }
+}
+
+static void print_ascii_values(Elf64_Shdr *section, void *data, int pos)
+{
+    unsigned char *byte = NULL;
+
+    for (size_t j = 0 ; j < 16 ; j++) {
+        if (j + pos >= section->sh_size) {
+            printf(" ");
+            continue;
+        }
+        byte = data + section->sh_offset + pos + j;
+        printf("%c", (*byte < 32 || *byte > 126) ? '.' : *byte);
+    }
+}
+
+void print_section(Elf64_Shdr *section, Elf64_Shdr *strtab, void *data)
+{
+    char *name = data + strtab->sh_offset + section->sh_name;
+
+    if (strlen(name) == 0)
+        return;
+    printf("Contents of section %s:\n", name);
+    for (size_t i = 0 ; i < section->sh_size ; i = i + 16) {
+        printf(" %04lx", section->sh_addr + i);
+        print_byte_values(section, data, i);
+        printf(" ");
+        print_ascii_values(section, data, i);
+        printf("\n");
+    }
+}
+
 int display_sections_content(void *data, char *filename)
 {
+    Elf64_Ehdr *header = data;
+    Elf64_Shdr *secs = data + header->e_shoff;
+    char *shstrtab = data + secs[header->e_shstrndx].sh_offset;
+
+    for (size_t i = 0 ; i < header->e_shnum ; i++) {
+        if (secs[i].sh_type != SHT_NOBITS && secs[i].sh_type
+                != SHT_SYMTAB && data + secs[i].sh_offset != (void*)shstrtab
+                && strcmp(&shstrtab[secs[i].sh_name], ".strtab") != 0)
+            print_section(&secs[i], &secs[header->e_shstrndx], data);
+    }
     return (0);
 }
